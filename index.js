cbc133b5efef6b5c7124270d86b369c6986d016f
Matthias Goetzke <m.goetzke@curasystems.de>
2015-04-17 09:43:21 +0200

    started test

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var indexContent = fs.readFileSync('index.html');
+var pdfContent = fs.readFileSync('pdf-sample.pdf');
+
+http.createServer(function (req, res) {
+  
+  if(req.url.indexOf('pdf-sample.pdf') >= 0) {
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="Test f%Cr us.pdf"');
+    res.setHeader('Content-Length', pdfContent.length);
+    res.setHeader('Connection', 'keep-alive');
+
+
+    res.end(pdfContent);
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(indexContent);
+  }
+}).listen(9615);
+
+console.log("INFO: Open http://localhost:9615")
