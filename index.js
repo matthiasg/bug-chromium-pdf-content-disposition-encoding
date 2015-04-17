12f4f9c9927e0f048f7e35217939d14aac3560c0
Matthias Goetzke <m.goetzke@curasystems.de>
2015-04-17 10:04:33 +0200

    added comment about ISO 8859-1

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ http.createServer(function (req, res) {
   
   if(req.url.indexOf('pdf-sample.pdf') >= 0) {
     res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename="Test f%Cr us.pdf"');
+
+    // NOTE ü and other german umlaute are contained in ISO 8859-1. See http://en.wikipedia.org/wiki/ISO/IEC_8859-1
+    res.setHeader('Content-Disposition', 'inline; filename="Test für UMLAUTE.pdf"');
+    // res.setHeader('Content-Disposition', contentDisposition( 'Test für us.pdf', {type:'inline'} ));
     res.setHeader('Content-Length', pdfContent.length);
     res.setHeader('Connection', 'keep-alive');
 
